a187fb728005c93c03c13a006663607b794f5fe1
Stefan Karasmilov <stefan.karasmilov@540deg.com>
2022-09-15 18:56:17 +0200

    step 0 solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+export const App = () => {
+  const [counter, setCounter] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={() => setCounter(prevState => ++prevState)} label="+1" />
+      <CounterDisplay counter={counter} />
+      <Button onClick={() => setCounter(prevState => --prevState)} label="-1" />
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = ({ label, onClick }) => {
+  return <button onClick={onClick}>{label}</button>
+}
+
+const CounterDisplay = ({ counter }) => {
+  return <p>{counter}</p>
+}
+
+
