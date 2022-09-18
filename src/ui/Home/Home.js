97ce4331072b732b6cd2c93aa4a09eaf12006417
Stefan Karasmilov <stefan.karasmilov@540deg.com>
2022-09-18 12:09:58 +0200

    step-1-solved done

--- src/ui/Home/Home.js
+++ src/ui/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 
 const comics = [
@@ -25,33 +25,64 @@ const comics = [
 ]
 
 export const Home = () => {
+  const [filter, setFilter] = useState('')
+
+  const filteredComics = comics.filter(comic => comic.title.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <main className="container">
-      <header>
-        <h1 className="title">
-          Buscador de cómics de Marvel
-        </h1>
-        <h2 className="subtitle">
-          Este buscador encontrará los cómics en los que aparezcan los dos personajes que selecciones en el formulario
-        </h2>
-      </header>
-      <section>
-        <p className="inputLabel">
-          Escribe un personaje en la lista
-        </p>
-        <div className="inputContainer">
-          <input className="filterInput" />
-          <button className="clearButton">Limpiar búsqueda</button>
-        </div>
-        {comics.map(comic => (
-          <div key={comic.id} className="comicCard">
-            <p className="comicTitle">
-              {comic.title}
-            </p>
-            <p>{comic.characters.join(', ')}</p>
-          </div>
-        ))}
-      </section>
+      <Header />
+      <ComicList comics={filteredComics} filter={filter} onFilter={setFilter} />
+      <Footer itemsCount={filteredComics.length} />
     </main>
   )
 }
+
+export const Header = () => {
+  return (
+    <header>
+      <h1 className="title">
+        Buscador de cómics de Marvel
+      </h1>
+      <h2 className="subtitle">
+        Este buscador encontrará los cómics en los que aparezcan los dos personajes que selecciones en el formulario
+      </h2>
+    </header>
+  )
+}
+
+export const ComicList = ({ filter, onFilter, comics }) => {
+  return (
+    <section>
+      <p className="inputLabel">
+        Escribe un personaje en la lista
+      </p>
+      <div className="inputContainer">
+        <input className="filterInput" onInput={e => onFilter(e.target.value)} value={filter} />
+        <button className="clearButton" onClick={() => onFilter('')}>Limpiar búsqueda</button>
+      </div>
+      {comics.map(comic => (
+        <div key={comic.id} className="comicCard">
+          <p className="comicTitle">
+            {comic.title}
+          </p>
+          <p>{comic.characters.join(', ')}</p>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export const Footer = ({ itemsCount }) => {
+  return (
+    <footer>
+      <p>Elementos en la lista: {itemsCount}</p>
+    </footer>
+  )
+}
+
+
+
+
+
+
