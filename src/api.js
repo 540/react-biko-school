b86de238d7a43018b0fc9ee7ee4cdfedbd3e232a
Moises C <fenderOne@users.noreply.github.com>
2022-10-03 13:46:28 +0200

    Update api.js
    The await expression include in the return statement is not necessary here!

--- src/api.js
+++ src/api.js
@@ -1,16 +1,18 @@
 export const api = {
   characters: async () => {
     const response = await fetch('/api/characters.json')
-    return await response.json()
+    // return await response.json()
+    // await is not necessary here!
+    return response.json()
   },
 
   comics: async (characterId) => {
     const response = await fetch(`/api/comics-${characterId}.json`)
-    return await response.json()
+    return response.json()
   },
 
   allComics: async () => {
     const response = await fetch('/api/comics.json')
-    return await response.json()
+    return response.json()
   }
 }
